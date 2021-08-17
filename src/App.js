538caa8b6e0d030da719c6bab488900b95fe7fa8
Zafer Kurucu <z.kurucu.d@hotmail.com>
2021-08-17 13:25:07 +0300

    Create App.css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
 import Weather from "./components/Weather";
-import { Container } from "semantic-ui-react";
+import React from "react";
+import { Card, Container, Dropdown, Segment } from "semantic-ui-react";
+
+const cityOptions = [
+  { key: 1, value: "Adana", text: "Adana" },
+  { key: 2, value: "Adıyaman", text: "Adıyaman" },
+  { key: 3, value: "Afyonkarahisar", text: "Afyonkarahisar" },
+];
 
 function App() {
   return (
-    <div>
-      <Container>
-        <Weather />
-      </Container>
-    </div>
+    <Container>
+      <h2>Weather App</h2>
+      <div className="city-dropdown">
+        <Dropdown
+          placeholder="Şehir Seçin"
+          search
+          selection
+          options={cityOptions}
+        />
+      </div>
+    </Container>
   );
 }
 
